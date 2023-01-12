55ec7f1ba4c16ce25e96a3870af1cc92c6d2703c
iahgo <iahgo@icloud.com>
2023-01-12 00:52:22 -0300

    req 10 OK

---
+++ src/models/BlogPost.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+    },
+    content: {
+      type: DataTypes.STRING,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+    published: {
+      type: DataTypes.DATE,
+    },
+    updated: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    timestamps: false,
+    underscored: true,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, { foreignKey: 'userId', as: 'user' });
+  };
+
+  return BlogPost;
+};
