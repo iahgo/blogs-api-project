de943449a3f7e55f44077883a456839af6442c00
iahgo <iahgo@icloud.com>
2023-01-12 00:07:52 -0300

    req 07 OK

---
+++ src/models/Category.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    timestamps: false,
+    underscored: true,
+  });
+
+  return Category;
+};
